77bab121d9e05312bf04fe7d75557b2cb73db094
AshishManer <108808771+ash44444@users.noreply.github.com>
2025-06-10 11:45:38 +0530

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,115 @@
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
+import { resetCart } from '../slices/cartSlice';
+//import './SearchBox.css'; // Import custom styles
+import { FaSearch } from 'react-icons/fa';
+import React, { useState } from 'react';
+
+
+
+const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [keyword, setKeyword] = useState('');
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      dispatch(resetCart());
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`);
+    } else {
+      navigate('/');
+    }
+  };
+
+  return (
+    <header>
+      <Navbar bg="warning" variant='dark' className="custom-navbar" expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to='/' style={{ fontSize: '24px', fontWeight: 'bold', color: '#000' }}>
+            SnapShop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto align-items-center'>
+
+              {/* Custom Styled SearchBox */}
+              <form onSubmit={submitHandler} className="styled-searchbox me-3">
+                <input
+                  type='text'
+                  name='q'
+                  onChange={(e) => setKeyword(e.target.value)}
+                  className='search-input'
+                  placeholder='Search...'
+                />
+                <button type='submit' className='search-button'>
+                  <FaSearch />
+                </button>
+              </form>
+
+              <Nav.Link as={Link} to='/cart'>
+                <FaShoppingCart /> Cart
+                {cartItems.length > 0 && (
+                  <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
+              </Nav.Link>
+
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <NavDropdown.Item as={Link} to='/profile'>
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <Nav.Link as={Link} to='/login'>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              )}
+
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <NavDropdown.Item as={Link} to='/admin/productlist'>
+                    Products
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/admin/orderlist'>
+                    Orders
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/admin/userlist'>
+                    Users
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
